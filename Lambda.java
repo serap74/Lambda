6553477c73d01b8431ba47b0acb190fda15b35cb
Serap Diraz <emeksevde74@gmail.com>
2022-04-27 08:58:25 +0300

    27.04.2022

---
+++ Lambda.java
@@ -0,0 +1,34 @@
+public class Lambda {
+
+    public static void main(String[] args) {
+
+        /*
+   1) Lambda "Functional Programming"-->mathod(action) kullanma pr dili.
+      Lambda --> mathod create  etme değil mevcut method'ları(library)secip kullanmaktır...
+      Lambda  kendi başına tanımlanabilen parametre alıp gönderebilen fonksiyonlardır.
+      Lambda'lar sayesinde daha az kod ve hızlı geliştirme sağlanabilmektedir.
+      Java 8 ile gelen bu özellik, Java 8’in en önemli özelliğidir.
+
+      "Functional Programming" de "Nasil yaparim?" degil "Ne yaparim?" dusunulur.
+   2) "Structured Programming" de "Ne yaparim?" dan cok "Nasil Yaparim?" dusunulur
+   3) "Functional Programming" hiz, code kisaligi, code okunabilirligi
+       ve hatasiz code yazma acilarindan cok faydalidir.
+   4) Lambda sadece collections'larda(List, Queue ve Set) ve array'lerde kullanilabilir ancak map'lerde kullanılmaz.
+      Lambda kullanmak hatasız code kullanmaktır.
+
+          Collections Nedir?
+          Çoğu yazılım tek tek öğeler yerine öğelerden oluşan toplulukları depolar ve onlar üzerinde işlem
+          yapar. Array'ler onlardan birisidir. Java Collections Framework, arraylerle yapılan işleri daha kolay
+          yaptığı gibi, daha fazlasını da yapar.
+          Java'da bir koleksiyon (collection - bazen container, ambar diye adlandırılır) nesnelerden oluşan bir
+          topluluğu bir arada tutan bir yapıdır. 'Collections Framework' ise arayüzler ve onların kurgularından
+          (implementations) oluşur.
+*/
+
+        List<Integer>sayi=new ArrayList<>(Arrays.asList(34,22,16,11,35,20,63,21,65,44,66,64,81,38));
+        printElStructured(sayi);
+    }
+
+    //Task: Structed programming kullanarak list elemanlarrini aralarinda bosluk olacak sekilde print ediniz
+
+}
